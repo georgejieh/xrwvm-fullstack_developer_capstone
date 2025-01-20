f326e5de80903812910dfa656d2ff115c109bb81
george.jieh <george.jieh@gmail.com>
2025-01-20 04:42:34 -0500

    Allow dealer_id to be null and change car year from date to int

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # CarMake model
 class CarMake(models.Model):
@@ -12,7 +13,7 @@ class CarMake(models.Model):
 # CarModel model
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
-    dealer_id = models.IntegerField()  # Refers to a dealer created in Cloudant database
+    dealer_id = models.IntegerField(null=True, blank=True, default=0)  # Optional with default value
     name = models.CharField(max_length=100)  # Name of the car model
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -20,7 +21,12 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon'),
     ]
     type = models.CharField(max_length=20, choices=CAR_TYPES, default='SEDAN')  # Type of the car model
-    year = models.DateField()  # Year of the car model
+    year = models.IntegerField(  # Store the year as an integer
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
 
     def __str__(self):
-        return f"{self.car_make.name} {self.name} ({self.type})"  # String representation
+        return f"{self.car_make.name} {self.name} ({self.year})"
