d6c4986413deb05896a82467cc6d1a53e089bd0e
georgejieh <99384826+georgejieh@users.noreply.github.com>
2025-01-20 17:49:23 -0800

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 # CarMake model
 class CarMake(models.Model):
     name = models.CharField(max_length=100)  # Name of the car make
@@ -12,19 +13,30 @@ class CarMake(models.Model):
 
 # CarModel model
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
-    dealer_id = models.IntegerField(null=True, blank=True, default=0)  # Optional with default value
+    car_make = models.ForeignKey(
+        CarMake, 
+        on_delete=models.CASCADE
+    )  # Many-to-One relationship
+    dealer_id = models.IntegerField(
+        null=True, 
+        blank=True, 
+        default=0
+    )  # Optional with default value
     name = models.CharField(max_length=100)  # Name of the car model
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
     ]
-    type = models.CharField(max_length=20, choices=CAR_TYPES, default='SEDAN')  # Type of the car model
+    type = models.CharField(
+        max_length=20, 
+        choices=CAR_TYPES, 
+        default='SEDAN'
+    )  # Type of the car model
     year = models.IntegerField(  # Store the year as an integer
         validators=[
             MaxValueValidator(2023),
-            MinValueValidator(2015)
+            MinValueValidator(2015),
         ]
     )
 
