f833e3d4810d458e8e4d9f6038804bcdc2a10d15
georgejieh <99384826+georgejieh@users.noreply.github.com>
2025-01-20 20:51:53 -0800

    Update 0001_initial.py

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -15,12 +15,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarMake',
             fields=[
-                ('id', models.BigAutoField(
-                    auto_created=True,
-                    primary_key=True,
-                    serialize=False,
-                    verbose_name='ID'
-                )),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID'
+                    )
+                ),
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField()),
             ],
@@ -28,28 +31,37 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarModel',
             fields=[
-                ('id', models.BigAutoField(
-                    auto_created=True,
-                    primary_key=True,
-                    serialize=False,
-                    verbose_name='ID'
-                )),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID'
+                    )
+                ),
                 ('dealer_id', models.IntegerField()),
                 ('name', models.CharField(max_length=100)),
-                ('type', models.CharField(
-                    choices=[
-                        ('SEDAN', 'Sedan'),
-                        ('SUV', 'SUV'),
-                        ('WAGON', 'Wagon')
-                    ],
-                    default='SEDAN',
-                    max_length=20
-                )),
+                (
+                    'type',
+                    models.CharField(
+                        choices=[
+                            ('SEDAN', 'Sedan'),
+                            ('SUV', 'SUV'),
+                            ('WAGON', 'Wagon'),
+                        ],
+                        default='SEDAN',
+                        max_length=20
+                    )
+                ),
                 ('year', models.DateField()),
-                ('car_make', models.ForeignKey(
-                    on_delete=django.db.models.deletion.CASCADE,
-                    to='djangoapp.carmake'
-                )),
+                (
+                    'car_make',
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to='djangoapp.carmake'
+                    )
+                ),
             ],
         ),
     ]
